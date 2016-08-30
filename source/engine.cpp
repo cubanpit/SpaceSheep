469ee78b3ee801713ab824b135e0dfe2d38d6053
cubanpit <cubanspit@gmail.com>
2016-08-30 22:00:22 +0200

    Splitted Engine::run() in different functions

--- source/engine.cpp
+++ source/engine.cpp
@@ -9,148 +9,39 @@
 Engine::Engine(unsigned int xDim, unsigned int yDim):m_artist(xDim,yDim)
 {
 	m_artist.WelcomeScreen();
+	sheep = new SpaceSheep(m_artist.get_xDim()/2,m_artist.get_yDim()-2-fatness,fatness);
 }
 
 void Engine::run()
 {
 	timeout(0); // getch() does not wait for input [ncurses]
 	erase();
+	bushes.clear(); //clear vector, if it's not empty	
 
 	m_artist.GameTable();
-	unsigned int fatness = 2;
-	SpaceSheep* sheep = new SpaceSheep(50,28-fatness,fatness);
 	m_artist.Pencil(sheep);
 	char ch; //needed for sheep movement
-	char left_mov = 'j', right_mov = 'l';
-	char pause = 'p';
 
-	/*
-	 * TIME STUFF
-	 */
+	// TIME STUFF
 	// WARNING: clocks goes on during execution, if you use sleep_until(20:00)
 	//  for obstacle and sleep(19:00) for sheep, the second will be ignored,
 	//  time has passed!
 	// SOLUTION: reassign time_point before using it, if necessary.
 	std::chrono::system_clock::time_point t_track_sheep = std::chrono::system_clock::now();
-	unsigned int dt_uint_obstacle = 150;
-	unsigned int dt_uint_sheep = 10; 
 	std::chrono::duration<int,std::milli> dt_sheep(dt_uint_sheep);
 
-	/*
-	 * ANIMATION AND HITBOX
-	 */
-	std::vector <RectObstacle*> bushes;
-
-	// Random stuff
-	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> distribution(0,m_artist.get_xDim());
-
-	unsigned int production = 14; //set vertical frequency of obstacle
-	unsigned int score = 0;
-	unsigned int count = 0; //counts refreshes
-	unsigned int w_d = 4; //margin between bushes distance and sheep width
-	unsigned int w_tot = 50; //minimal total width of bushes on the same row
-	unsigned int w_m = 20; //minimal width of bushes
-	unsigned int w_r = 22; //range of width, w_m+(w_r-1)=maximum width
-	unsigned int h_m = 3; //minimal height of bushes
-	unsigned int h_r = 4; //range of height
-	unsigned int w = 0, h = 0;
-	int x = 0, y = 0;
-	bool ctrl = false; //to check obstacle spawn
+	unsigned int count = 0;
 	bool dead = false;
 	bool new_game = true;
-	/*
-	 * In the next 'while(!ctrl)' cycle we check different parameters to
-	 *  have a good game environment.
-	 * RectObstacle:
-	 * - size limits for x,y,h,w at creation time
-	 * - limits on (x+w) to have every obstacle inside Game Table
-	 * - minimum horizontal distance between obstacle, to be sure
-	 *    the SpaceSheep can pass (m_dist)
-	 * - minimum tot with of obstacle on same row, so it's always
-	 *    engaging (tot_w)
-	 * - check obstacle overlapping
-	 *   
-	 * WARNING:
-	 * It's not so simple to respect all this rules.
-	 * Minimum width (m_w) and maximum width (M_w) values modify the
-	 *  solution to the problem.
-	 *
-	 * GameLimit           center              GameLimit
-	 * |                   |                   |
-	 * |                |obst |free|  obst     |
-	 * |                ,-----,----,-----------|
-	 * |                m_w   |    X           |
-	 *                        m_dist
-	 *
-	 * In this situation X must be m_w<=X<=M_w and w_min+X=tot_w
-	 * Idem with M_w instead of m_w in the scheme.
-	 *
-	 * This simple 'if' statement check that conditions can't cause a loop in
-	 *  obstacle creation.
-	 */
-	if (   !((((int)m_artist.get_xDim()/2) - ((int)w_m/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) > w_m 
-		 and (((int)m_artist.get_xDim()/2) - ((int)w_m/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) < ((int)w_m+(int)w_r-1)
-		 and (((int)m_artist.get_xDim()/2) - (((int)w_m+int(w_r)-1)/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) > w_m 
-		 and (((int)m_artist.get_xDim()/2) - (((int)w_m+int(w_r)-1)/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) < ((int)w_m+(int)w_r-1)) ) {
-		std::cerr << "Engine::run() ERROR: minimum and range of obstacle's width can cause an infinite loop, change their value." << std::endl;
-		dead = true;
-	}
+	
+	dead = check_bushes_parameters();
 
 	while (!dead) {
 		if ( !(count%production) ) {
-			for ( unsigned int i=0; i<2; ++i) {
-				RectObstacle* tmp_bush = new RectObstacle(x,y,w,h);
-				while (!ctrl) {
-					w = (distribution(generator) % w_r) + w_m; //(dist%range)+min
-					x = distribution(generator);
-					if ( (x + w) < (int)m_artist.get_xDim() ) ctrl = true;
-					if ( i > 0 ) {
-						if ( ctrl ) {
-							if ( ((x >= (((*(bushes.back())).get_ref()).x + (int)((*(bushes.back())).get_rec()).width)) and 
-										(x - (((*(bushes.back())).get_ref()).x +
-											  (int)((*(bushes.back())).get_rec()).width)) <
-										((int)(*sheep).get_fatness()*2+1+(int)w_d)) ) ctrl = false;
-							if ( (((x+(int)w) <= ((*(bushes.back())).get_ref()).x) and 
-										(((*(bushes.back())).get_ref()).x - (x + (int)w)) <
-										((int)(*sheep).get_fatness()*2+1+(int)w_d)) ) ctrl = false;
-						}
-						if ( ctrl ) {
-							if ( (int)((*(bushes.back())).get_rec()).width+(int)w < w_tot ) {
-								ctrl = false;
-							}
-						}
-					}
-
-					h = (distribution(generator) % h_r) + h_m ;
-					y = -h ;
-					RectObstacle* t_b = new RectObstacle(x,y,w,h);
-					delete tmp_bush;
-					tmp_bush = t_b;
-
-					if ( ctrl ) {
-						for (auto it = bushes.begin(); it != bushes.end(); it++) {
-							if ( ((*(*it)).m_hitbox).Overlap((*tmp_bush).m_hitbox) ) {
-								ctrl = false;
-								break;
-							}
-						}
-					}
-				}
-				ctrl = false; //reset control bool
-				bushes.push_back(tmp_bush); //only good obstacle are stored
-
-				// Old obstacles need to be erased from the vector
-				for (auto it = bushes.begin(); it != bushes.end(); it++) {
-					if ( ((*(*it)).get_ref()).y > (int)m_artist.get_yDim() ) {
-						delete *it;
-						bushes.erase(it);
-					}
-				}
-			}
+			add_obstacle_bushes();	
 		}
 		++count;
+		//compute score, different weight for different difficulty level
 		score = count*(200/(production+(w_d*10)+(dt_uint_obstacle/10)));
 
 		for (auto it = bushes.begin(); it != bushes.end(); it++) {
@@ -164,11 +55,11 @@ void Engine::run()
 		for (int i=0; i<dt_uint_obstacle and !dead; i+=dt_uint_sheep) {
 			ch = getch();
 			if ( ch == left_mov and  (((*sheep).get_ref()).x - 
-							(int)(*sheep).get_fatness()) > 1 ) {
+						(int)(*sheep).get_fatness()) > 1 ) {
 				m_artist.Animation(sheep,0);
 			}
 			else if ( ch == right_mov and (((*sheep).get_ref()).x + 
-					  (int)(*sheep).get_fatness()) < ((int)m_artist.get_xDim() - 2) ) {
+						(int)(*sheep).get_fatness()) < ((int)m_artist.get_xDim() - 2) ) {
 				m_artist.Animation(sheep,1);
 			}
 			else if ( ch == pause ) {
@@ -190,7 +81,6 @@ void Engine::run()
 					break;
 				}
 			}
-			//print score, different weight for different difficulty level
 			m_artist.Score(score);
 			refresh();
 			t_track_sheep += dt_sheep;
@@ -202,3 +92,107 @@ void Engine::run()
 	if ( new_game ) run();
 }
 
+void Engine :: add_obstacle_bushes ()
+{
+	/*
+	 * In the next 'while(!ctrl)' cycle we check different parameters to
+	 *  have a good game environment.
+	 * RectObstacle:
+	 * - size limits for x,y,h,w at creation time
+	 * - limits on (x+w) to have every obstacle inside Game Table
+	 * - minimum horizontal distance between obstacle, to be sure
+	 *    the SpaceSheep can pass (m_dist)
+	 * - minimum tot with of obstacle on same row, so it's always
+	 *    engaging (tot_w)
+	 * - check obstacle overlapping
+	 *   
+	 * WARNING:
+	 * It's not so simple to respect all these rules.
+	 * Minimum width (m_w) and maximum width (M_w) values modify the
+	 *  solution to the problem.
+	 *
+	 * GameLimit           center              GameLimit
+	 * |                   |                   |
+	 * |                |obst |free|  obst     |
+	 * |                ,-----,----,-----------|
+	 * |                m_w   |    X           |
+	 *                        m_dist
+	 *
+	 * In this situation X must be m_w<=X<=M_w and w_min+X=tot_w
+	 * Idem with M_w instead of m_w in the scheme.
+	 *
+	 * This simple 'if' statement check that conditions can't cause a loop in
+	 *  obstacle creation.
+	 */
+
+	// Random stuff
+	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed);
+	std::uniform_int_distribution<int> distribution(0,m_artist.get_xDim());
+
+	unsigned int w = 0, h = 0;
+	int x = 0, y = 0;
+	bool ctrl = false; //to check obstacle spawn
+
+	for ( unsigned int i=0; i<2; ++i) {
+		RectObstacle* tmp_bush = new RectObstacle(x,y,w,h);
+		while (!ctrl) {
+			w = (distribution(generator) % w_r) + w_m; //(dist%range)+min
+			x = distribution(generator);
+			if ( (x + w) < (int)m_artist.get_xDim() ) ctrl = true;
+			if ( i > 0 ) {
+				if ( ctrl ) {
+					if ( ((x >= (((*(bushes.back())).get_ref()).x + (int)((*(bushes.back())).get_rec()).width)) and 
+								(x - (((*(bushes.back())).get_ref()).x +
+									  (int)((*(bushes.back())).get_rec()).width)) <
+								((int)(*sheep).get_fatness()*2+1+(int)w_d)) ) ctrl = false;
+					if ( (((x+(int)w) <= ((*(bushes.back())).get_ref()).x) and 
+								(((*(bushes.back())).get_ref()).x - (x + (int)w)) <
+								((int)(*sheep).get_fatness()*2+1+(int)w_d)) ) ctrl = false;
+				}
+				if ( ctrl ) {
+					if ( (int)((*(bushes.back())).get_rec()).width+(int)w < w_tot ) {
+						ctrl = false;
+					}
+				}
+			}
+
+			h = (distribution(generator) % h_r) + h_m ;
+			y = -h ;
+			RectObstacle* t_b = new RectObstacle(x,y,w,h);
+			delete tmp_bush;
+			tmp_bush = t_b;
+
+			if ( ctrl ) {
+				for (auto it = bushes.begin(); it != bushes.end(); it++) {
+					if ( ((*(*it)).m_hitbox).Overlap((*tmp_bush).m_hitbox) ) {
+						ctrl = false;
+						break;
+					}
+				}
+			}
+		}
+		ctrl = false; //reset control bool
+		bushes.push_back(tmp_bush); //only good obstacle are stored
+
+		// Old obstacles need to be erased from the vector
+		for (auto it = bushes.begin(); it != bushes.end(); it++) {
+			if ( ((*(*it)).get_ref()).y > (int)m_artist.get_yDim() ) {
+				delete *it;
+				bushes.erase(it);
+			}
+		}
+	}
+}
+
+bool Engine :: check_bushes_parameters ()
+{
+	if (   !((((int)m_artist.get_xDim()/2) - ((int)w_m/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) > w_m 
+				and (((int)m_artist.get_xDim()/2) - ((int)w_m/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) < ((int)w_m+(int)w_r-1)
+				and (((int)m_artist.get_xDim()/2) - (((int)w_m+int(w_r)-1)/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) > w_m 
+				and (((int)m_artist.get_xDim()/2) - (((int)w_m+int(w_r)-1)/2) - ((int)(*sheep).get_fatness()*2+1+(int)w_d)) < ((int)w_m+(int)w_r-1)) ) {
+		std::cerr << "Engine::run() ERROR: minimum and range of obstacle's width can cause an infinite loop, change their value." << std::endl;
+		return true;
+	}
+	else return false;
+}
--- source/engine.h
+++ source/engine.h
@@ -18,11 +18,28 @@ class Engine
 		~Engine(){}
 
 		void run();
+		void add_obstacle_bushes();
+		bool check_bushes_parameters();
 	
 	private:
 		Sketcher m_artist;
+		SpaceSheep* sheep;
+		std::vector<RectObstacle*> bushes;
 
-
+		unsigned int fatness = 2;
+		unsigned int production = 14; //set vertical frequency of obstacle
+		unsigned int dt_uint_obstacle = 150;
+		unsigned int w_d = 4; //margin between bushes distance and sheep width
+		unsigned int w_tot = 50; //minimal total width of bushes on the same row
+		unsigned int w_m = 20; //minimal width of bushes
+		unsigned int w_r = 22; //range of width, w_m+(w_r-1)=maximum width
+		unsigned int h_m = 3; //minimal height of bushes
+		unsigned int h_r = 4; //range of height	
+		
+		unsigned int dt_uint_sheep = 10; 
+		unsigned int score = 0;
+		char left_mov = 'j', right_mov = 'l';
+		char pause = 'p';
 };
 
 #endif // _ENGINE_H_
